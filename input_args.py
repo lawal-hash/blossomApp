94dd5825ff41fa43141152879b03e596cda05886
Sophia Lawal <lawalsophia@gmail.com>
2023-07-16 22:09:00 +0200

    feat: Retrieves and parse command line arguments

---
+++ input_args.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+def get_users_args():
+    parser = argparse.ArgumentParser()    
+    parser.add_argument('input', type=str, default='/', help='path to test image')
+    parser.add_argument('data_directory', type=str, default='flowers/', help='path to folder of images')
+    parser.add_argument('checkpoint', type=bool, default=True, help='Save the model state_dict after training, highly encouraged')
+    
+    parser.add_argument('--arch',type = str, default='squeezenet', help = 'Type of pre-trained neural network architecture to use, not fully supported')
+    parser.add_argument('--checkpoint_path', type=str, default='checkpoint.pth', help='path to save the checkpoint')    
+    parser.add_argument('--top_k', type=int, default=5, help='Return K most likely class')
+    parser.add_argument('--category_names', type=str, default='cat_to_name.json', help='path to folder of mapping of categories to the real names')
+    parser.add_argument('--device', type=str, default='cpu', help='enable gpu training or cpu training, gpu is highly recommended')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='path to folder of images')
+    parser.add_argument('--epochs', type=int, default=5, help='number of times to train the model')
+    
+    parser.add_argument('--train_batchsize', type=int, default=64, help='training batch size')
+    parser.add_argument('--test_batchsize', type=int, default=64, help='test batch size')
+    
+    
+    
+    
+    return parser.parse_args()
